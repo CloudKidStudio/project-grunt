3a28f6ea1d6c633883ec9b5112179b4608eea88b
Matt Bargar <mattbargar@cloudkid.com>
2014-08-28 14:53:09 -0400

    added audio folder scaffolding

--- scripts/preinstall.js
+++ scripts/preinstall.js
@@ -51,8 +51,12 @@ scaffold("Gruntfile.js", null, function(file){
 	// Create the required folders
 	scaffoldDir("src"); 
 	scaffoldDir("deploy"); 
-	scaffoldDir("deploy/assets"); 
+	scaffoldDir("deploy/assets");
 	scaffoldDir("deploy/assets/css"); 
+	scaffoldDir("deploy/assets/sound"); 
+	scaffoldDir("deploy/assets/sound/vo"); 
+	scaffoldDir("deploy/assets/sound/sfx"); 
+	scaffoldDir("deploy/assets/sound/music"); 
 	scaffoldDir("deploy/logic");
 
 	// Copy the required files
