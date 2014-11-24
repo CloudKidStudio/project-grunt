2349137ac6ea24d5c14526d8292931218ec0c37d
Matt Karl <matt@cloudkid.com>
2014-11-24 16:17:49 -0500

    Updated dependencies

--- tasks/version.js
+++ tasks/version.js
@@ -2,15 +2,6 @@ module.exports = {
 	options : {
 		'build.json' : 'version',
 		'bower.json' : 'version',
-		'deploy/index.html' : function(contents, version){
-			return contents.replace(
-					/src\=(\"|\')([^\?\n\r\"\']+)(\?v\=[a-z0-9\.]*)?(\"|\')/ig, 
-					'src="$2?v='+version+'"'
-				)
-				.replace(
-					/href\=(\"|\')([^\?\n\r\"\']+\.css)(\?v\=[a-z0-9\.]*)?(\"|\')/ig, 
-					'href="$2?v='+version+'"'
-				);
-		}
+		'deploy/index.html' : 'cache-bust'
 	}
 };
